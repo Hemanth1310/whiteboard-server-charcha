1d8120200065454977c865898125cdb885bfd796
Hemanth1310 <hemanth13k@gmail.com>
2024-06-06 22:23:19 +0200

    6june-1020

--- server.js
+++ server.js
@@ -5,25 +5,29 @@ const io = require('socket.io')(http);
 // Store active rooms
 const activeRooms = {};
 // Store disposed rooms
-const disposedRooms = [];
+const disposedRooms = new Set();
+// Store drawing data for each room
+const roomDrawings = {};
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('A user connected:', socket.id);
 
   // Join or create room
   socket.on('joinOrCreateRoom', (roomId) => {
-    if (disposedRooms.includes(roomId)) {
+    if (disposedRooms.has(roomId)) {
       // If room is disposed, notify the client
-      console.log(`User ${socket.id} accessed disposed room ${roomId}`);
+      console.log(`User ${socket.id} attempted to access disposed room ${roomId}`);
       socket.emit('roomClosed', roomId);
     } else {
       if (!activeRooms[roomId]) {
         // Create a new room
         activeRooms[roomId] = [];
+        roomDrawings[roomId] = []; // Initialize drawing data for the room
       }
       socket.join(roomId);
       activeRooms[roomId].push(socket.id);
       socket.emit('roomJoined', roomId); // Emit roomJoined event here
+      socket.emit('loadDrawing', roomDrawings[roomId]); // Send existing drawing data to the new user
       console.log(`User ${socket.id} joined room ${roomId}`);
     }
   });
@@ -32,7 +36,8 @@ io.on('connection', (socket) => {
   socket.on('drawing', (data) => {
     const { roomId, point } = data;
     if (activeRooms[roomId]) {
-      io.to(roomId).emit('drawing', { x: point.x, y: point.y });
+      roomDrawings[roomId].push(point); // Store drawing data
+      io.to(roomId).emit('drawing', point);
       console.log(`Drawing event received from user ${socket.id} in room ${roomId}:`, point);
     }
   });
@@ -45,7 +50,8 @@ io.on('connection', (socket) => {
       console.log(`User ${socket.id} left room ${roomId}`);
       if (activeRooms[roomId].length === 0) {
         delete activeRooms[roomId]; // If no users in room, dispose it
-        disposedRooms.push(roomId); // Add to disposed rooms list
+        disposedRooms.add(roomId); // Add to disposed rooms list
+        delete roomDrawings[roomId]; // Delete drawing data for the room
         console.log(`Room ${roomId} has been disposed of`);
         io.emit('roomDisposed', roomId);
       }
@@ -61,7 +67,8 @@ io.on('connection', (socket) => {
         activeRooms[roomId] = activeRooms[roomId].filter(id => id !== socket.id);
         if (activeRooms[roomId].length === 0) {
           delete activeRooms[roomId]; // If no users in room, dispose it
-          disposedRooms.push(roomId); // Add to disposed rooms list
+          disposedRooms.add(roomId); // Add to disposed rooms list
+          delete roomDrawings[roomId]; // Delete drawing data for the room
           console.log(`Room ${roomId} has been disposed of`);
           io.emit('roomDisposed', roomId);
         }
